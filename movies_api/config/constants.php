b657c5c10afbb337285117a3b516e9bf6574851d
aurel <aurel@pcaurel.com>
2026-04-08 08:49:42 +0200

    Ajout du fichier de config API

---
+++ movies_api/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// On récupère la clé directement depuis Windows
+$sysApiKey = getenv('IMDB_API_KEY');
+
+if (!$sysApiKey) {
+    $sysApiKey = $_SERVER['IMDB_API_KEY'] ?? null;
+}
+
+// Définition des constantes
+define('IMDB_API_KEY', $sysApiKey);
+define('IMDB_API_URL', 'http://www.omdbapi.com/');
+
+// Vérification de sécurité pour ton dossier de maintenance
+if (empty(IMDB_API_KEY)) {
+    error_log("ALERTE MAINTENANCE : Clé API introuvable dans le système.");
+}
+
+define('APP_NAME', 'Books & Movies API');
+?>
